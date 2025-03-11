0233fd28f393d5c85e72e2cc60f1c00c0da81112
igantius <ignatpaxomov@gmail.com>
2025-03-11 15:17:13 +0300

    dorabotki

--- src/commands/AddIfMin.java
+++ src/commands/AddIfMin.java
@@ -1,43 +1,35 @@
 package commands;
 
+import body.*;
 import collection.CollectionWorker;
 import manager.UserManager;
+import utillity.IDGenerator;
 import utillity.Printer;
 
 public class AddIfMin extends Command {
     public AddIfMin(String description, boolean hasArgs, UserManager userManager, CollectionWorker workerCollection) {
         super(description, hasArgs, userManager, workerCollection);
     }
-
     @Override
     public void execute(Printer printer) {
         if (checkArgument(new Printer(), getArgs())) {
-            if (collection.getCollection().isEmpty()) {
-                printer.print("Коллекция уже пустая");
-            } else {
-                collection.getCollection().clear();
-                printer.print("Коллекция очищена!");
-            }
+            Worker worker = new Worker();
+            worker.setId(IDGenerator.generateUniqueId());
+            userManager.userDataCollect(worker);
+            collection.addWorker(worker);
+            printer.print("Объект успешно добавлен в коллекцию!");
+
         }
     }
-
 
     @Override
     public boolean checkArgument(Printer printer, Object inputArgs) {
         if (inputArgs == null) {
-            return true;
-        } else {
-            printer.print("У команды clear нет аргументов!");
+            printer.print("У команды AddIFMin должен быть аргумент endDate!");
             return false;
         }
+        return true;
     }
 
-
 }
-
-
-
-
-
-
 
